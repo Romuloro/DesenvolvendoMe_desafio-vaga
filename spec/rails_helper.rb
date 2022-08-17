2deb1cae1f471adaaeccf0e6facba1163c68ff3e
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-17 16:32:04 -0300

    feat(configure database): clean up each test event

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,9 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'support/database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -37,7 +38,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  #config.use_transactional_fixtures = true
 
   config.include FactoryBot::Syntax::Methods
 
@@ -59,8 +60,11 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  config.use_transactional_fixtures = false
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
 end

0656e83ae205f1624e8079767fc65ecbe4da2798
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-18 20:59:48 -0300

    test(book): requests tests and book facture

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :book do
     titulo { FFaker::Lorem.word }
     published_at { FFaker::Time.datetime }
-    author { build(:author) }
+    author { create(:author) }
   end
 end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -17,115 +17,159 @@ RSpec.describe "/books", type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Book. As you add validations to Book, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let!(:book) {create(:book)}
+  let!(:book_attributes) {attributes_for(:book)}
 
   describe "GET /index" do
-    it "renders a successful response" do
-      Book.create! valid_attributes
-      get books_url
-      expect(response).to be_successful
+    context "when the book exists"  do
+      before do
+        get "/books/"
+      end
+
+      it "renders a successful response" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders books titulo" do
+        expect(response.body).to include(book.titulo)
+      end
+
+      it "renders books Published at" do
+        expect(response.body).to include(book.published_at.to_s)
+      end
+      it "renders books author name" do
+        expect(response.body).to include(book.author.name)
+      end
+
     end
   end
 
   describe "GET /show" do
-    it "renders a successful response" do
-      book = Book.create! valid_attributes
-      get book_url(book)
-      expect(response).to be_successful
+    context "when the book exists"  do
+      before do
+        get "/books/", params: {id:book.id}
+      end
+
+      it "renders a successful response" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders books titulo" do
+        expect(response.body).to include(book.titulo)
+      end
+
+      it "renders books Published at" do
+        expect(response.body).to include(book.published_at.to_s)
+      end
+      it "renders books author name" do
+        expect(response.body).to include(book.author.name)
+      end
+    end
+
+    context "when the book doesn't exists" do
+      let!(:book_error) {build(:book)}
+      before do
+        get "/books/", params: {id:book_error.id}
+      end
+
+      it "not renders book titulo" do
+        expect(response.body).to_not include(book_error.titulo)
+      end
     end
   end
 
   describe "GET /new" do
     it "renders a successful response" do
-      get new_book_url
+      get "/books/new"
       expect(response).to be_successful
     end
   end
 
   describe "GET /edit" do
-    it "renders a successful response" do
-      book = Book.create! valid_attributes
-      get edit_book_url(book)
-      expect(response).to be_successful
+    context "when the books exists"  do
+      before do
+        get "/books/#{book.id}/edit"
+      end
+
+      it "renders a successful response" do
+        expect(response).to be_successful
+      end
+
+      it "renders books titulo" do
+        expect(response.body).to include(book.titulo)
+      end
+
+      it "renders books Published at" do
+        expect(response.body).to include(book.published_at.to_datetime.to_s[0..-7])
+      end
+      it "renders books author name" do
+        expect(response.body).to include(book.author.name)
+      end
     end
   end
 
   describe "POST /create" do
     context "with valid parameters" do
+      before do
+        post "/books/", params: { book: { titulo: book.titulo, published_at: book.published_at.to_datetime.to_s[0..-7], author_id: book.author.id}}
+
+      end
+
       it "creates a new Book" do
-        expect {
-          post books_url, params: { book: valid_attributes }
-        }.to change(Book, :count).by(1)
+        expect(response).to have_http_status(:redirect)
       end
 
       it "redirects to the created book" do
-        post books_url, params: { book: valid_attributes }
-        expect(response).to redirect_to(book_url(Book.last))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "does not create a new Book" do
-        expect {
-          post books_url, params: { book: invalid_attributes }
-        }.to change(Book, :count).by(0)
-      end
-
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post books_url, params: { book: invalid_attributes }
-        expect(response).to be_successful
+        get "/books", params: { id: book.id }
+        expect(response.body).to include("Book was successfully created.")
       end
     end
   end
 
   describe "PATCH /update" do
     context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested book" do
-        book = Book.create! valid_attributes
-        patch book_url(book), params: { book: new_attributes }
-        book.reload
-        skip("Add assertions for updated state")
+      before do
+        patch "/books/#{book.id}", params: { book: book_attributes }
       end
 
       it "redirects to the book" do
-        book = Book.create! valid_attributes
-        patch book_url(book), params: { book: new_attributes }
-        book.reload
-        expect(response).to redirect_to(book_url(book))
+        expect(response).to have_http_status(:redirect)
       end
-    end
 
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        book = Book.create! valid_attributes
-        patch book_url(book), params: { book: invalid_attributes }
-        expect(response).to be_successful
+      it "redirects to the update book" do
+        get "/books", params: { id: book.id }
+        expect(response.body).to include("Book was successfully updated.")
+      end
+
+      it "redirects to the update book titulo" do
+        get "/books/#{book.id}"
+        expect(response.body).to include(book.reload.titulo.to_s)
+      end
+      it "redirects to the update book published at" do
+        get "/books/#{book.id}"
+        expect(response.body).to include(book.reload.published_at.to_s)
+      end
+      it "redirects to the update book author name" do
+        get "/books/#{book.id}"
+        expect(response.body).to include(book.reload.author.name.to_s)
       end
     end
   end
 
   describe "DELETE /destroy" do
-    it "destroys the requested book" do
-      book = Book.create! valid_attributes
-      expect {
-        delete book_url(book)
-      }.to change(Book, :count).by(-1)
-    end
+    context "with valid parameters" do
+      before do
+        delete "/books/#{book.id}"
+      end
 
-    it "redirects to the books list" do
-      book = Book.create! valid_attributes
-      delete book_url(book)
-      expect(response).to redirect_to(books_url)
+      it "destroys the requested author" do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "redirects to the delete author" do
+        get "/books", params: { id: book.id }
+        expect(response.body).to include("Book was successfully destroyed.")
+      end
     end
   end
 end

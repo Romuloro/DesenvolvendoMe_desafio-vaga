00cdaa336291352da3186c4b1c87dfb6d42c11db
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-20 13:30:41 -0300

    feat(Account): generate scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_150553) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_162820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "account_number"
+    t.bigint "supplier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -29,5 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_150553) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "accounts", "suppliers"
   add_foreign_key "books", "authors"
 end

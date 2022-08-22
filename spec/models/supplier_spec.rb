fe3cbf296748d01d20f418a385bbbbed9b044a29
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-22 18:25:14 -0300

    test(Account and Book): Test Model

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'valid supplier' do
+    supplier = build(:supplier)
+    expect(supplier).to be_valid
+  end
 end

52e89f94017041d74ef98cca9eadc32c931b7b04
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-08-22 17:49:23 -0300

    feat(Account): database
    change account number - unique and hexadecimal number

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,10 @@
+require 'securerandom'
+
 class Account < ApplicationRecord
   belongs_to :supplier
+  validates_uniqueness_of :account_number
+
+  def random_account_number
+    self.account_number = SecureRandom.hex
+  end
 end

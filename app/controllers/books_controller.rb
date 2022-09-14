7b08a55b61f831ebf46068c6829d6c5ba348cf89
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-09-14 11:47:20 -0300

    feat(books): api create, delete, and update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /books or /books.json
   def index
@@ -26,7 +27,7 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
+        format.json { render json: {message: "Book #{@book.titulo} was successfully created."}, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -39,7 +40,7 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
+        format.json { render json: {message: "Book #{@book.titulo} was successfully updated."}, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -53,7 +54,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
+      format.json {render json: {message: "Book #{@book.titulo} was successfully destroyed."}, status: :ok}
     end
   end
 
